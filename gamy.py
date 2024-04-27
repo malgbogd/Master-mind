85019f4f07ff917f34a74b970a066379241b80ed
edus4ever <eduszurawek@gmail.com>
2024-04-27 11:42:38 +0200

    liczby od usera

--- gamy.py
+++ gamy.py
@@ -11,3 +11,8 @@ def draw_elements():
     return elements
 
 print(draw_elements())
+
+print("sprobuj odgadnac haslo, podaj 4 liczby np. 1,2,3,4")
+liczby_uzytkownika=input()
+print("wprowadzone liczby to")
+print(liczby_uzytkownika)
