ff72a41a2905755c557d91ea0e164c83ff3b7043
malgbogd <malgbogd@gmail.com>
2024-04-27 11:19:57 +0200

    draw random 4 unique numbers sequence

--- gamy.py
+++ gamy.py
@@ -1,1 +1,13 @@
 print("Test")
+
+import random
+
+def draw_elements():
+    elements=[]
+    while len(elements)<4:
+        number = random.randint(1,8)
+        if number not in elements:
+            elements.append(number)
+    return elements
+
+print(draw_elements())
